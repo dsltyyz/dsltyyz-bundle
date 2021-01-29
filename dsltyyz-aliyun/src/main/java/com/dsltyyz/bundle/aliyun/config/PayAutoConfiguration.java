78952cd567141826087487d5195778f4695f93d3
yangyuanliang <yangyuanliang@dsltyyz.com>
2021-01-29 17:38:00 +0800

    导出数据字典 增加为空 默认值

--- dsltyyz-aliyun/src/main/java/com/dsltyyz/bundle/aliyun/config/PayAutoConfiguration.java
+++ dsltyyz-aliyun/src/main/java/com/dsltyyz/bundle/aliyun/config/PayAutoConfiguration.java
@@ -1,9 +1,7 @@
 package com.dsltyyz.bundle.aliyun.config;
 
 import com.dsltyyz.bundle.aliyun.client.pay.AliyunPayClient;
-import com.dsltyyz.bundle.aliyun.client.sms.AliyunSmsClient;
 import com.dsltyyz.bundle.aliyun.common.properties.PayProperties;
-import com.dsltyyz.bundle.aliyun.common.properties.SmsProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Description:
  * 条件注入PayProperties属性和Pay客户端
- *
  *
  * @author: dsltyyz
  * @date: 2020/08/27
@@ -23,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 public class PayAutoConfiguration {
 
     @Bean
-    public AliyunPayClient aliyunPayClient(){
+    public AliyunPayClient aliyunPayClient() {
         return new AliyunPayClient();
     }
 
