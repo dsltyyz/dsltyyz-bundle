43360af707d3e2e83c5e02a7f8a596d26b43a1be
yangyuanliang <yangyuanliang@dsltyyz.com>
2021-09-17 15:59:23 +0800

    提升版本2.0.0-SNAPSHOT=>2.1.0-SNAPSHOT 分页排序 排序字段转换工具

---
+++ dsltyyz-template/src/main/java/com/dsltyyz/bundle/template/util/PageUtil.java
@@ -0,0 +1,92 @@
+package com.dsltyyz.bundle.template.util;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.dsltyyz.bundle.template.page.PageDTO;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * Description:
+ * Page工具
+ *
+ * @author: dsltyyz
+ * @date: 2021-9-17
+ */
+public class PageUtil {
+
+    /**
+     * 映射转换
+     * @param pageDTO
+     * @param map
+     */
+    public static void convertOrderItem(PageDTO pageDTO, Map<String, String> map) {
+        //排序列表不为空 且map转换不为空
+        if (pageDTO.getOrders() != null && pageDTO.getOrders().size() != 0 && map != null && map.size() > 0) {
+            pageDTO.getOrders().forEach(orderItem -> {
+                //只替换map中的字段 没有的保留原字段
+                if (map.containsKey(orderItem.getColumn())) {
+                    orderItem.setColumn(map.get(orderItem.getColumn()));
+                }
+            });
+        }
+    }
+
+    /**
+     * 实体类注解转换
+     * @param pageDTO
+     * @param entityClass
+     */
+    public static void convertOrderItem(PageDTO pageDTO, Class entityClass) {
+        //排序列表不为空
+        if (pageDTO.getOrders() != null && pageDTO.getOrders().size() != 0) {
+            pageDTO.getOrders().forEach(orderItem -> {
+                try {
+                    Field declaredField = entityClass.getDeclaredField(orderItem.getColumn());
+                    boolean annotationPresent = declaredField.isAnnotationPresent(TableId.class);
+                    if (annotationPresent) {
+                        orderItem.setColumn(declaredField.getAnnotation(TableId.class).value());
+                    } else {
+                        orderItem.setColumn(declaredField.getAnnotation(TableField.class).value());
+                    }
+                } catch (NoSuchFieldException e) {
+                    return;
+                }
+            });
+        }
+    }
+
+    /**
+     * 下划线转换
+     * @param pageDTO
+     */
+    public static void convertOrderItem(PageDTO pageDTO) {
+        //排序列表不为空 且map转换不为空
+        if (pageDTO.getOrders() != null && pageDTO.getOrders().size() != 0) {
+            pageDTO.getOrders().forEach(orderItem -> {
+                orderItem.setColumn(underscoreName(orderItem.getColumn()));
+            });
+        }
+    }
+
+    private static String underscoreName(String name) {
+        StringBuilder result = new StringBuilder();
+        if (name != null && name.length() > 0) {
+            // 将第一个字符处理成大写
+            result.append(name.substring(0, 1).toLowerCase());
+            // 循环处理其余字符
+            for (int i = 1; i < name.length(); i++) {
+                String s = name.substring(i, i + 1);
+                // 在大写字母前添加下划线
+                if (s.equals(s.toUpperCase()) && !Character.isDigit(s.charAt(0))) {
+                    result.append("_");
+                }
+                // 其他字符直接转成大写
+                result.append(s.toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+
+}
