91902699534a3f073ea787bb80065c8cef38beda
yangyuanliang <yangyuanliang@dsltyyz.com>
2021-12-13 15:26:35 +0800

    隐藏数据

---
+++ dsltyyz-common/src/main/java/com/dsltyyz/bundle/common/data/helper/DateHideDataHelper.java
@@ -0,0 +1,22 @@
+package com.dsltyyz.bundle.common.data.helper;
+
+/**
+ * 日期数据处理
+ *
+ * @author dsltyyz
+ */
+public class DateHideDataHelper implements DataHelper {
+
+    /**
+     * 日期数据处理
+     *
+     * @param o
+     * @param param
+     * @return
+     */
+    @Override
+    public Object deal(Object o, String param) {
+        return null;
+    }
+
+}
